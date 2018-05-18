405151e4f10ab5bc81ee8d0b3fa5bf2f77029945
Thanh Nguyen <ngtanthanh90@gmail.com>
2018-05-18 09:30:24 +0000

    Fix Keyboard 180518

--- admin/database/config.inc.php
+++ admin/database/config.inc.php
@@ -3,5 +3,5 @@
 define('DB_SERVER', "localhost");           // Database Host
 define('DB_USER', "root");                  // Databse User
 define('DB_PASS', "Tanthanh");                      // Database Password
-define('DB_DATABASE', "teamta_bot");   		// Database Name
+define('DB_DATABASE', "test_bot");   		// Database Name
 ?>
